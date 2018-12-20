f9a61752567f5ae520d33daee183f681425ca998
okm02 <okm02@mail.com>
2018-12-20 14:23:42 +0100

    added loading and tutorial

--- myiris/js/modal.js
+++ myiris/js/modal.js
@@ -1,20 +1,17 @@
 
-function browse_shortcuts(){
-
-
-	pause_iris();
-	play_restart_counter +=1;	
+function browse_shortcuts(){	
 
 	// Get the modal
 	var modal = document.getElementById('myModal');
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 	modal.style.display = "block";
-	
+
+	pause_iris();	
+
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
 	 	modal.style.display = "none";
-	  	play_restart_counter +=1;
 		pause_iris()
 	}
 
@@ -23,19 +20,50 @@ function browse_shortcuts(){
 
 function browse_fileload(){
 
-
-	pause_iris()
-	play_restart_counter +=1;	
-
 	var second = document.getElementById('fileModal');
 	var span = document.getElementsByClassName("close")[1];
 	second.style.display = "block";
 
+	pause_iris();
+
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
+	  document.getElementsByClassName("drag")[0].innerHTML = '<p> Drag File here</p>';
 	  second.style.display = "none";
-	  play_restart_counter +=1;
 	  pause_iris();
 	}
 
 }
+
+
+function handleFileSelect(evt){
+
+	
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files; // FileList object.
+    document.getElementsByClassName("drag")[0].innerHTML = '<p>' + files[0].name + '</p>';  
+
+}
+
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
